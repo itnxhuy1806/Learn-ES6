5fb2066bb461d1a1565c5c623e76fb31b6614741
Ngô Xuân Huy - 60135752 <itnxhuy1806@gmail.com>
2022-03-16 11:51:29 +0700

    edit comment & add log

--- 16 - ES6 Rest Parameters.js
+++ 16 - ES6 Rest Parameters.js
@@ -1,28 +1,26 @@
 function myFunc() {
-     console.log(arguments);
+     console.log(arguments); // arguments is object
 }
 
 myFunc(1, 2, 3)
-//[object Arguments] {
-//  0: 1,
-//  1: 2,
-//  2: 3
-//}
+console.log("------------");
 function Store() {
      var aisle = {
           fruit: [],
           vegetable: []
      }
      return {
-          //Store().add('category', 'item1', 'item2');
-           add: function(category, ...items) {
-          //   var items = [].splice.call(arguments, 1);
-             console.log(items); // ["apples", "oranges"]
-             items.forEach(function(value, index, array) {
-               aisle[category].push(value);
-             });
-           },
-       }
+          add: function (category, ...items) {
+               console.log(arguments);
+               var items = [].splice.call(arguments, 1);
+               console.log("run [].splice.call()");
+               console.log(arguments);
+               console.log(items); // ["apples", "oranges"]
+               items.forEach(function (value, index, array) {
+                    aisle[category].push(value);
+               });
+          },
+     }
 }
 
 var myGroceryStore = new Store();
