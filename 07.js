606ef995c6a18e8eadc78f3b4159003c4d081c84
Ngô Xuân Huy - 60135752 <itnxhuy1806@gmail.com>
2022-03-16 07:20:43 +0700

    Update 07.js

--- 07.js
+++ 07.js
@@ -1,6 +1,10 @@
 let a1 = [1,2,3];
 let a2 = [4,5,6];
 console.log(...a1);
+
+let _a1 = a1; //cùng tham chiêu vs a1
+let __a1 = [...a1]; // khác tham chiếu (copy a1)
+
 {
      let a1 = [1,2,3];
      a1.push(a2);
